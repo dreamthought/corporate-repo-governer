bc950d990035791d359c567aea08d05f1ded04d5
Raf Gemmail <rafiq@dreamthought.com>
2019-02-11 11:46:45 +1300

    Spelling fixup: s/governer/governor/

--- repo-name-governor/src/test/java/com/largecorp/RepoNameGovernorAppTests.java
+++ repo-name-governor/src/test/java/com/largecorp/RepoNameGovernorAppTests.java
@@ -1,4 +1,4 @@
-package com.example.reponamegoverner;
+package com.largecorp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class RepoNameGovernorAppTests {
 
 	@Test
 	public void contextLoads() {
