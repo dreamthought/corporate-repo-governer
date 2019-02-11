26918f8840a97880c0e6967364afe7f1e5ac1dae
Raf Gemmail <rafiq@dreamthought.com>
2019-02-11 22:12:11 +1300

    Fixed up blind test writing typos

--- webhook-delegator/app/models/github_event.rb
+++ webhook-delegator/app/models/github_event.rb
@@ -1,5 +1,5 @@
 require 'json'
-
+require 'pp'
 # Represents an event consumed from GitHub
 # FIXME: use a json inflated entity or jsonpath
 class GithubEvent
@@ -8,18 +8,18 @@ class GithubEvent
     @@REPO_NAME_KEY = "full_name"
 
     def initialize(githubEvent)
-        @json = JSON.parse(githubEvent)
+        @json = JSON.parse(githubEvent.to_s)
     end
 
     # Since our bounded context is particularly concerned with
     # repos being created
-    def isRepositoryCreateEvent? 
-        @json.has_key?( @@ADD_REPO_KEY ) && @json[ @@ADD_REPO_KEY ].length >= 1;
+    def isRepositoryCreateEvent?
+        return @json.has_key?(@@ADD_REPO_KEY) && @json[ @@ADD_REPO_KEY ].length >= 1;
     end
 
     # returns the name of the first created repo in this event
     def getRepositoryName
-        if (self.isRepositoryCreate?)
+        if (self.isRepositoryCreateEvent?)
             # we assume there is only one repo per change set for
             # the purpose of this change set.
             # This can be confirmed alter
