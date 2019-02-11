9b2157cd53a786fef801754f995668eb7bf102f2
Raf Gemmail <rafiq@dreamthought.com>
2019-02-11 22:11:23 +1300

    Collaboration between rails endpoint and collaborator service

--- webhook-delegator/app/controllers/webhook_controller.rb
+++ webhook-delegator/app/controllers/webhook_controller.rb
@@ -1,10 +1,25 @@
+require("pp")
+require_relative("../models/github_event")
+require_relative("../services/name_governance_client")
+
 class WebhookController < ApplicationController
+    include NameGovernanceClient
 
     # Simple echo to validate
     def create
         @body = request.body.read
-        puts @body
-    end
 
+        @event = GithubEvent.new(@body)
+
+        if @event.isRepositoryCreateEvent?
+            # Notify the name governance service
+            sendNotification(@event.getRepositoryName)
+        end
+
+        # allow response inspection
+        if ENV["SNOOP"]
+            pp @body
+        end
+    end
 end
 
