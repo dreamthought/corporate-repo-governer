250e746b16f0ff4a81cd9849e9e0224905e00c8f
Raf Gemmail <rafiq@dreamthought.com>
2019-02-11 19:04:17 +1300

    JWT generation and name validation

--- repo-name-governor/src/main/java/com/largecorp/RepoNameGovernorApp.java
+++ repo-name-governor/src/main/java/com/largecorp/RepoNameGovernorApp.java
@@ -2,8 +2,15 @@ package com.largecorp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@Profile("!async-disabled") // for testing
+/**
+ * An application intended to validate repository names in accordance with internal governance at LargeCorp Inc
+ */
 public class RepoNameGovernorApp {
 
 	public static void main(String[] args) {
