2d9c4c55647e3546163340031f7675aa3faf694f
Raf Gemmail <rafiq@dreamthought.com>
2019-02-11 22:11:23 +1300

    Collaboration between rails endpoint and collaborator service

--- webhook-delegator/spec/controllers/webhook_spec.rb
+++ webhook-delegator/spec/controllers/webhook_spec.rb
@@ -3,37 +3,22 @@ require 'rails_helper'
 
 RSpec.describe 'Webhook Delegator Service', :type => :request do
 
-    def stub_governer
-      stub_request(:post, "http://repository-governer/repository/check").
+    before(:each) do
+       stub_request(:post, "http://repo-name-governor:8080/repository/name/dreamthought/repo_creation_webhook_test").
          with(
+           body: "{\"repositoryName\":\"dreamthought/repo_creation_webhook_test\"}",
            headers: {
-              'Accept'=>'*/*',
-              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-              'Host'=>'repository-governer',
-              'User-Agent'=>'Ruby'
-           }).to_return(status: 202, body: "", headers: {})
+       	  'Content-Type'=>'application/json; charset=utf-8'
+           }).
+         to_return(status: 202, body: "", headers: {})
     end
-
 
     describe 'POST /webhook', :type => :request do
         context 'when we have a well-formed and understood request' do
             it 'returns a no-content response' do
-                post '/webhook', params: file_fixture("create-repo-installed-app.json").read.to_json
+                post '/webhook', params: file_fixture("create-repo-installed-app.json").read
                 expect(response).to have_http_status(204)
             end
-
-            it 'requests a collabor asynchronously checks the repository name' do
-                post '/webhook', params: file_fixture("create-repo-installed-app.json").read.to_json
-                uri = URI('http://repository-governer/repository/check')
-                req = Net::HTTP::Post.new(uri)
-                res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-                    http.request(req, '{ "repository": "myorg/project1" }')
-            
-                    response = Net::HTTP.post(uri)
-                end
-
-            end
-
         end
     end
 
