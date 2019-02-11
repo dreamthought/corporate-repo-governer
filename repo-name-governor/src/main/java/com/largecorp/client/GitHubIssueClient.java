5435a914ea1d760f4d5b3f307ff10854cf95d85f
Raf Gemmail <rafiq@dreamthought.com>
2019-02-12 03:18:17 +1300

    Model shape of Github Issue

--- repo-name-governor/src/main/java/com/largecorp/client/GitHubIssueClient.java
+++ repo-name-governor/src/main/java/com/largecorp/client/GitHubIssueClient.java
@@ -1,6 +1,9 @@
 package com.largecorp.client;
 
+import com.largecorp.model.GitHubIssue;
 import com.largecorp.service.GitHubJWTProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -16,9 +19,14 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GitHubIssueClient {
 
+    Logger logger = LoggerFactory.getLogger(GitHubIssueClient.class);
+
     @Autowired
     GitHubJWTProvider jwtProvider;
 
+
+
+    private static final String TITLE = "REPOSITORY STANDARDS VIOLATION DETECTED";
 
     // FIXME this should really be pulled from the API but skipping due to time
     private static final String ISSUE_URL_FORMAT = "https://api.github.com/repos/%s/issues";
@@ -26,18 +34,33 @@ public class GitHubIssueClient {
     /**
      * Creates an issue posting to URL in line
      * with githubs provided contract
-     * @param url
-     * @param issue
+     * @param repository name
+     * @param issueDetail to be posted
      */
-    public void createIssue(String repository, String issue) {
+    public void createIssue(String repository, String issueDetail) {
         String createUrl = String.format(ISSUE_URL_FORMAT, repository);
+        GitHubIssue issue = GitHubIssue.builder().title(TITLE)
+            .body(issueDetail).build();
+
+        sendIssue(createUrl, issue);
+    }
+
+    private void sendIssue(String createUrl, GitHubIssue issue) {
+        logger.info("Creating an issue at url:", createUrl);
         RestTemplate rest = new RestTemplate();
-        HttpHeaders httpHeaders = new HttpHeaders();
-        String jwt = jwtProvider.getJwt();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        HttpEntity<String> request = new HttpEntity<>(httpHeaders);
+        HttpEntity<GitHubIssue> request = makeRequest(issue);
+
         ResponseEntity<String> response = rest.postForEntity(
             createUrl, request , String.class);
+    }
+
+    private HttpEntity<GitHubIssue> makeRequest(GitHubIssue issue) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        String jwt = jwtProvider.getJwt();
+        httpHeaders.add("Authorization", "Bearer " + jwt);
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
+
+        return new HttpEntity<GitHubIssue>(issue, httpHeaders);
     }
 
 
--- repo-name-governor/src/main/java/com/largecorp/model/GitHubIssue.java
+++ repo-name-governor/src/main/java/com/largecorp/model/GitHubIssue.java
@@ -1,4 +1,14 @@
 package com.largecorp.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * A minimal DITO for GitHub Issue Creation
+ */
+@Data
+@Builder
 public class GitHubIssue {
+    String title;
+    String body;
 }
